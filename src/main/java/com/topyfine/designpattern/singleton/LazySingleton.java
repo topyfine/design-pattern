109662e242422b9f78c38fa37a550b2ca3c57091
Young <young_fine@163.com>
2018-11-10 12:54:07 +0800

    单例模式

--- src/main/java/com/topyfine/designpattern/singleton/LazySingleton.java
+++ src/main/java/com/topyfine/designpattern/singleton/LazySingleton.java
@@ -3,10 +3,7 @@ package com.topyfine.designpattern.singleton;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 import java.util.stream.Stream;
 
 /**
@@ -52,10 +49,9 @@ public class LazySingleton {
      * @return 单例
      */
     public static synchronized LazySingleton getInstance2() {
-        System.out.println("[线程安全][线程尝试进入...]" + Thread.currentThread().getName());
         if (instance == null) {
             try {
-                System.out.println("[线程安全][进入成功]" + Thread.currentThread().getName());
+                System.out.println("[线程安全]" + Thread.currentThread().getName());
                 Thread.sleep(500);
             } catch (InterruptedException e) {
                 e.printStackTrace();
@@ -65,34 +61,56 @@ public class LazySingleton {
         return instance;
     }
 
-    public static void main(String[] args) {
-
-        List<String> list = Collections.synchronizedList(new ArrayList<>(100));
+    public static void main(String[] args) throws InterruptedException {
+        int taskNum = 1000 * 1000;
         // 首先验证非线程安全的方法
         ThreadPoolExecutor pool = new ThreadPoolExecutor(10, 10, 5,
-                TimeUnit.SECONDS, new ArrayBlockingQueue<>(90), Executors.defaultThreadFactory());
+                TimeUnit.SECONDS, new ArrayBlockingQueue<>(2 * taskNum), Executors.defaultThreadFactory());
+
         // 非线程安全
+        List<String> list = Collections.synchronizedList(new ArrayList<>(taskNum));
+        CountDownLatch latch = new CountDownLatch(taskNum);
         Runnable task = () -> {
             LazySingleton instance = getInstance();
-            System.out.println(instance);
+//            System.out.println(instance);
             list.add(instance.toString());
+            latch.countDown();
         };
-        /*for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < taskNum; i++) {
             pool.execute(task);
-        }*/
+        }
+        // 线程阻塞于此，等待所有线程完成
+        latch.await();
         // 查看实例是否唯一
-        /*System.out.println(list.size());
-        long count = Stream.of(list).distinct().count();
-        System.out.println(count);*/
+        System.out.println(list.size());
+        long count = Stream.of(list.toArray()).distinct().count();
+        System.out.println(count);
+        if (count > 1) {
+            System.err.println("[非线程安全]单例不唯一");
+        }
 
         // 线程安全
+        /*List<String> list2 = Collections.synchronizedList(new ArrayList<>(taskNum));
+        CountDownLatch latch2 = new CountDownLatch(taskNum);
         Runnable task2 = () -> {
             LazySingleton instance = getInstance2();
-            System.out.println(instance);
+//            System.out.println(instance);
+            list2.add(instance.toString());
+            latch2.countDown();
         };
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < taskNum; i++) {
             pool.execute(task2);
         }
+        // 线程阻塞于此，等待所有线程完成
+        latch2.await();
+        // 查看实例是否唯一
+        System.out.println(list2.size());
+        long count2 = Stream.of(list2.toArray()).distinct().count();
+        System.out.println(count2);
+        if (count2 > 1) {
+            System.err.println("[线程安全]单例不唯一");
+        }*/
+
         pool.shutdown();
     }
 }
